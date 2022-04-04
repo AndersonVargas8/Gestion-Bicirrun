1afdc9bd5a4f166e112acfb22dde7d93c02c15c7
mvilladas <mvilladas@unal.edu.com>
2022-04-03 19:27:37 -0500

    Agregada navbar.

---
+++ src/main/java/com/app/springapp/controller/InicioController.java
@@ -0,0 +1,13 @@
+package com.app.springapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InicioController {
+    @GetMapping("/inicio")
+    public String inicio( Model model){
+        return "inicio";
+    }
+}
